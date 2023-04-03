2e374ac9d9f0bdc17394a34651a0f928e97ab7a9
Mando <mando@pop-os.localdomain>
2023-04-03 16:23:56 -0300

    my resume of this initial configuration

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ app.get("/", function (req, res) {
 app.listen(5000, function () {
   console.log("Servidor escutando na porta 5000");
 });
+
+
+// the var express requires (calls) the express framework to the actual document and the "express" var
+
+// the var app calls the express function to the ¨app" var
+
+
+// app.get is the GET method, app is the express framework
+
+// listen makes it works on port 5000
