a954e648ae784acf112a662aa0133a22ce49d4aa
maxxcleiton <maxcleiton@live.com>
2023-04-06 13:49:37 -0300

    router adds

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var mongoose = require("mongoose");
 
+var productsRouter = require('./routes/productsRoute');
+
 var app = express();
+var router = express.Router();
 
 var url =
   "mongodb+srv://gamatesu123:9naXlNX9H6ZUV3Dh@restapi.q0azbva.mongodb.net/test";
@@ -20,6 +23,27 @@ app.listen(5000, function () {
   console.log("Server listening on port 5000");
 });
 
+app.use('/products', productsRouter);
+
+router.get('/', function(req,res) {
+  res.send('This is the home page!');
+});
+
+router.get('/teste', function(res,res) {
+  res.send('This is the /test page!')
+});
+
+app.use('/', router);
+
+app.get('/', function (req,res) {
+  res.send('Welcome to Express!');
+});
+
+app.get('/teste', function (req, res) {
+  res.send('Replying /test solicitation');
+});
+
+
 // the var express requires (calls) the express framework to the actual document and the "express" var
 
 // the var app calls the express function to the ¨app" var
@@ -31,3 +55,6 @@ app.listen(5000, function () {
 // app.get("/", function (req, res) {
 //   res.send("Bem-vindo ao express!");
 // });
+
+//Source: https://macoratti.net/17/04/node_restmgdb4.htm
+//https://macoratti.net/17/04/node_restmgdb5.htm
